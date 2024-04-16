a3f089204118f66ac313a7e99febbb600550339e
sokrates2424 <148720498+sokrates2424@users.noreply.github.com>
2024-04-16 18:12:10 +0100

    Update script.js

--- numberGuesser/script.js
+++ numberGuesser/script.js
@@ -2,8 +2,6 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
 const generateTarget = () => Math.floor(Math.random() * 10)
 
 const compareGuesses = (humanGuess,computerGuess,targetNum) => {

64535c114623ca0e83b9cb291c8058a08ca66fd2
sokrates2424 <148720498+sokrates2424@users.noreply.github.com>
2024-04-16 18:10:22 +0100

    Add files via upload

---
+++ numberGuesser/script.js
@@ -0,0 +1,34 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => Math.floor(Math.random() * 10)
+
+const compareGuesses = (humanGuess,computerGuess,targetNum) => {
+    if(Math.abs(targetNum - humanGuess) <= Math.abs(targetNum - computerGuess)) {
+            return true
+    } else if(Math.abs(targetNum - humanGuess) > Math.abs(targetNum - computerGuess)) {
+            return false
+    } else {
+        return log.console('Sorry something went wrong, please try again.')
+    }
+}    
+
+function updateScore(roundWinner) {
+    if (roundWinner === 'human') {
+            humanScore ++ 
+    } else if (roundWinner === 'computer') {
+            computerScore ++
+    } else {
+            console.log('Something went wrong! The winner argument could not be read.');
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber ++
+}
+
+
+
